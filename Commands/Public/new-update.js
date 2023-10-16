b2d9196c13890844923e21e55a488e0f2df86734
ItsElectroax <128041548+ItsElectroax@users.noreply.github.com>
2023-10-16 16:46:45 +0200

    Update new-update.js

--- Commands/Public/new-update.js
+++ Commands/Public/new-update.js
@@ -11,11 +11,11 @@ module.exports = {
         .setTitle(`(NEW) Update log v${version}`)
         .setDescription('Check out what is new in this update of Multi!')
         .addFields(
-            {name: 'Bug fixes', value: '- `/player` has been fixed!'},
-            {name: 'What\s new?', value: '- `/search` command added to music!'},
-            {name: 'Upcoming updates', value: '- Moderation tools Coming Soon™!\n- `/rewind` and `/fast-forward` commands coming to music!'}
+            {name: 'Bug fixes', value: '- The bot will no longer leave the current VC to join the other one (This was supposed to be an initial feature, but it never worked)'},
+            {name: 'What\s new?', value: '- NONE'},
+            {name: 'Upcoming updates', value: '- `/rewind` and `/fast-forward` commands coming to music!'}
         )
-        .setFooter({text: '06/10/2023'})
+        .setFooter({text: '16/10/2023'})
         interaction.reply({embeds: [embed]})
     }
 }
